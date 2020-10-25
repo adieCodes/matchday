fa64ce1019f6cbd215c7f65019cee17a10930845
Adie Williams <hello@adie.codes>
2020-10-25 17:39:32 +0000

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+export const endOfLine = 'auto';
+export const printWidth = 80;
+export const singleQuote = true;
+export const semi = true;
+export const tabWidth = 2;
+export const trailingComma = 'es5';
+export const jsxSingleQuote = true;
+export const bracketSpacing = true;

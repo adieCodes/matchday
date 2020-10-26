095e493f35ef0a36229d1a47a653e6b16cbdc502
Adie Williams <hello@adie.codes>
2020-10-26 20:20:18 +0000

    Add correct class to Loading component

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import loading from '../loading.svg';
 
-const Loading = () => (
-  <img src={loading} className='App-logo' alt='logo' width='100%' />
-);
+const Loading = () => <img src={loading} className='loading' alt='logo' />;
 
 export default Loading;

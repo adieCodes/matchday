bcf58a4cdcc8a4e5d1fc6a76d0f48ab7352993de
Adie Williams <hello@adie.codes>
2020-10-26 17:19:12 +0000

    Fetch data, render dynamic contestants and add Loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Scoreboard from './Components/Scoreboard';
+import Loading from './Components/Loading';
 import './App.css';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [contestants, setContestants] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://odds-api.checkd-dev.com/dev/smartacca/fixtures/88f0tj3y6neqz20nsrameokyc/preview'
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setContestants(res.match.contestant);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className='app'>
-      <Scoreboard />
+      {isLoading ? <Loading /> : <Scoreboard contestants={contestants} />}
     </div>
   );
 }
--- src/Components/Scoreboard.tsx
+++ src/Components/Scoreboard.tsx
@@ -1,36 +1,47 @@
 import React from 'react';
 
-const Scoreboard = () => (
-  <table className='scoreboard'>
-    <caption className='scoreboard__competition'>Premier League</caption>
-    <tbody className='scoreboard__scoreboard'>
-      <tr className='scoreboard__home'>
-        <td className='scoreboard__badge--home'>
-          <img
-            className='scoreboard__home-badge scoreboard__badge'
-            src='https://s3.eu-west-1.amazonaws.com/live.content.fantasyiteam.com/bethub/teams/150x150/b496gs285it6bheuikox6z9mj.png'
-            alt='Aston Villa Badge'
-          />
-        </td>
-        <td className='scoreboard__teams--home'>Aston Villa</td>
-        <td className='scoreboard__time' rowSpan={2}>
-          FT
-        </td>
-        <td className='scoreboard__score--home scoreboard__score'>7</td>
-      </tr>
-      <tr className='scoreboard__away'>
-        <td className='scoreboard__badge--away'>
-          <img
-            className='scoreboard__away-badge scoreboard__badge'
-            src='https://s3.eu-west-1.amazonaws.com/live.content.fantasyiteam.com/bethub/teams/150x150/c8h9bw1l82s06h77xxrelzhur.png'
-            alt='Liverpool FC Badge'
-          />
-        </td>
-        <td className='scoreboard__teams--away'>Liverpool</td>
-        <td className='scoreboard__score--away scoreboard__score'>2</td>
-      </tr>
-    </tbody>
-  </table>
-);
+interface Props {
+  contestants: Team[];
+}
+
+const Scoreboard = (props: Props) => {
+  const { contestants } = props;
+  const teamBadges = [
+    'https://s3.eu-west-1.amazonaws.com/live.content.fantasyiteam.com/bethub/teams/150x150/b496gs285it6bheuikox6z9mj.png',
+    'https://s3.eu-west-1.amazonaws.com/live.content.fantasyiteam.com/bethub/teams/150x150/c8h9bw1l82s06h77xxrelzhur.png',
+  ];
+  // Can't think of a better descriptor for whether a team is at home or away
+  const venue = ['home', 'away'];
+
+  return (
+    <table className='scoreboard'>
+      <caption className='scoreboard__competition'>Premier League</caption>
+      <tbody className='scoreboard__scoreboard'>
+        {contestants.map((contestant, i) => (
+          <tr className={`scoreboard__${venue[i]}`} key={`${venue[i]}-score`}>
+            <td className={`scoreboard__badge--${venue[i]}`}>
+              <img
+                className={`scoreboard__${venue[i]}-badge scoreboard__badge`}
+                src={teamBadges[i]}
+                alt={`${contestant.name} Badge`}
+              />
+            </td>
+            <td className={`scoreboard__teams--${venue[i]}`}>
+              {contestants[0].name}
+            </td>
+            {i === 0 && (
+              <td className='scoreboard__time' rowSpan={2}>
+                FT
+              </td>
+            )}
+            <td className={`scoreboard__score--${venue[i]} scoreboard__score`}>
+              7
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Scoreboard;
